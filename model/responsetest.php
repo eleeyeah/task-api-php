c87297c03430ce3b6515a2eeb0a9f7d654f386e4
eleeyeah <ilietmks@gmail.com>
2023-02-15 20:36:48 +0100

    test Response

---
+++ model/responsetest.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('Response.php');
+
+$response = new Response();
+
+$response->setSuccess(true); // set the success property to true
+
+$response->setHttpStatusCode(200); // set the http status code to 200
+
+$response->addMessage("Message 1"); // add a message to the messages array
+
+$response->addMessage("Message 2"); // add a message to the messages array
+
+$response->send(); // send the response
